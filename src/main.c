acaefe8934072fcd5851f2dbe9ac7d5aa469ecae
DanielKool <daniel@familiekool.nl>
2025-10-09 13:34:13 +0200

    set up main

--- src/main.c
+++ src/main.c
@@ -2,3 +2,20 @@
 #include <avr/interrupt.h>
 
 #include <twi.h>
+
+int main(void)
+{
+    // Initialize TWI (I2C) as a slave with address 0x42
+    twi_init_slave(0x42);
+
+    // Enable global interrupts
+    sei();
+
+    while (1)
+    {
+        // Main loop can perform other tasks
+        // TWI events are handled in the interrupt service routines
+    }
+
+    return 0;
+}
