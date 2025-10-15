f70211d88d6679bcb9856742553e91e5d3c51483
DanielKool <daniel@familiekool.nl>
2025-10-15 16:26:06 +0200

    Revert "main still need filters, lcd and 7seg"
    This reverts commit b3d2ced13db45c0409561d2fbbc19ce542a74234.

--- src/main.c
+++ src/main.c
@@ -5,53 +5,48 @@
 #include "usart.h"
 #include "ultrasonic.h"
 #include "buzzer.h"
+
 #include "adc.h"
 
-// --- Configuratieconstanten ---
 #define FREQ_MIN 230
 #define FREQ_MAX 1400
 #define MAX_DISTANCE_CM 65
 
 int main(void)
 {
-    // Initialisaties
     USART_Init();
     Ultrasonic_Init();
     Buzzer_Init();
+    Filter_Init(5);
     ADC_Init();
 
-    sei(); // interrupts aanzetten
+    sei();
 
     while (1)
     {
-        // Trigger een afstandsmeting
         Ultrasonic_Trigger();
         _delay_ms(60);
 
-        // Als de meting klaar is
         if (Ultrasonic_IsReady())
         {
             uint16_t distance = Ultrasonic_GetDistance();
+            Filter_AddValue(distance);
+            uint16_t filtered = Filter_GetMedian();
 
-            // Limiteer afstand
-            if (distance > MAX_DISTANCE_CM)
+            if (filtered > MAX_DISTANCE_CM)
             {
-                distance = MAX_DISTANCE_CM;
+                filtered = MAX_DISTANCE_CM;
             }
 
-            // Bereken frequentie binnen bereik
-            uint16_t frequency = FREQ_MAX - (distance * (FREQ_MAX - FREQ_MIN) / MAX_DISTANCE_CM);
+            uint16_t frequency = FREQ_MAX - (filtered * (FREQ_MAX - FREQ_MIN) / MAX_DISTANCE_CM);
             Buzzer_SetFrequency(frequency);
 
-            // Lees volume (ADC potmeter)
             uint8_t volume = ADC_GetValue();
-            OCR2B = volume; // PWM duty-cycle aanpassen voor volume
+            OCR2B = volume; // volume instellen
 
-            // Toon waarden via USART
             char buffer[64];
             sprintf(buffer, "Afstand: %ucm | Freq: %uHz | Vol: %u\r\n",
-                    distance, frequency, volume);
-
+                    filtered, frequency, volume);
             for (char *p = buffer; *p; p++)
             {
                 USART_Transmit(*p);
